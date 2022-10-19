ffae946e8e1c958acf6abe1dac1ee3ed529116dd
sekiseiT <1901015448ec@cyber-u.ac.jp>
2022-10-19 23:17:02 +0900

    second commit

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+  <?php 
+    $result = "";
+    if(isset($_POST['add'])) {
+      $result = "登録しました";
+    }
+    echo $result;
+  ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>ボタンを押すと反応</title>
 </head>
 <body>
-  <?php echo "Hello World"?>
+  <form action="index.php" method="post">
+    <button type="submit" name = "add">登録</button>
+  </form>
 </body>
 </html>
