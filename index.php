3786ac4fa7425aeb5716099ee72caebedf8824f4
sekiseiT <1901015448ec@cyber-u.ac.jp>
2022-10-20 18:10:34 +0900

    Add css file, show project name

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
-  <?php 
-    $result = "";
-    if(isset($_POST['add'])) {
-      $result = "登録しました";
-    }
-    echo $result;
-  ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ボタンを押すと反応</title>
+  <title>TODOリスト</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <form action="index.php" method="post">
-    <button type="submit" name = "add">登録</button>
+  <h1>プロジェクトの作成</h1>
+  <hr/>
+  <form method="GET" action="index.php">
+    <input type="text" name="title" size="50">
+    <input type="submit" name="add_project" value="プロジェクトの作成">
   </form>
+
+  <div id="project">
+
+    <?php 
+    $project_name = "";
+    if(isset($_GET["add_project"])) {
+      $project_name = $_GET["title"];
+    }
+    echo $project_name;
+    ?>
+  </div>
 </body>
 </html>
